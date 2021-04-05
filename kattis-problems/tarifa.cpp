76e8552e7221bdfc59ef74477c205ce6f3c017f6
Domonic Tafoya <dtafoya17@live.com>
2021-04-05 11:59:35 -0600

    updated tarifa

--- kattis-problems/tarifa.cpp
+++ kattis-problems/tarifa.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    int mbPerMonth, months, mbSpent, mbLeft;
+
+    cin >> mbPerMonth >> months;
+
+    mbLeft = mbPerMonth * (months + 1);
+
+    for (int i = 0; i < months; i++) {
+        cin >> mbSpent;
+        mbLeft -= mbSpent;
+    }
+
+    cout << mbLeft;
+
     return 0;
 }
